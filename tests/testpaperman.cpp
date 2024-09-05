e8ce216fe01febe7389601f01cd02b11d8e6885b
Simon Glass <sjg@chromium.org>
2024-09-04 19:26:17 -0600

    mainwidget: Notice possible missing folders
    When searching for possible folder matches, return a list of folders
    that may be missing, i.e. that would produce a better match if they
    existed.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tests/testpaperman.cpp
+++ tests/testpaperman.cpp
@@ -43,33 +43,83 @@ void TestPaperman::testDetectMonth()
 
 void TestPaperman::testDetectMatches()
 {
-   QStringList matches, final;
+   QStringList matches, final, missing;
    QDate date = QDate(2024, 9, 1);
 
-   final = utilDetectMatches(date, matches);
+   final = utilDetectMatches(date, matches, missing);
    QCOMPARE(final.size(), 0);
+   QCOMPARE(missing.size(), 0);
 
    // Should ignore a month if it isn't preceeded by a year
    matches << "bills/03mar";
-   final = utilDetectMatches(date, matches);
+   final = utilDetectMatches(date, matches, missing);
    QCOMPARE(final.size(), 0);
+   QCOMPARE(missing.size(), 0);
 
    matches << "bills/2024/03mar";
-   final = utilDetectMatches(date, matches);
+   final = utilDetectMatches(date, matches, missing);
    QCOMPARE(final.size(), 0);
+   QCOMPARE(missing.size(), 0);
 
    // Check it can detect a month
    matches << "bills/2024/09sep";
-   final = utilDetectMatches(date, matches);
+   final = utilDetectMatches(date, matches, missing);
    QCOMPARE(final.size(), 1);
    QCOMPARE(final[0], "bills/2024/09sep");
+   QCOMPARE(missing.size(), 0);
 
    // Check it can detect a year
    matches << "bills/2024";
-   final = utilDetectMatches(date, matches);
+   final = utilDetectMatches(date, matches, missing);
    QCOMPARE(final.size(), 2);
-   QCOMPARE(final[0], "bills/2024/09sep");  // should be in other order
+   QCOMPARE(final[0], "bills/2024/09sep");
    QCOMPARE(final[1], "bills/2024");
+   QCOMPARE(missing.size(), 0);
+
+   // Advance to Oct 2024; check it can suggest adding a month
+   date = QDate(2024, 10, 1);
+   final = utilDetectMatches(date, matches, missing);
+   QCOMPARE(final.size(), 1);
+   QCOMPARE(final[0], "bills/2024");
+   QCOMPARE(missing.size(), 1);
+   QCOMPARE(missing[0], "bills/2024/10oct");
+
+   // Make sure it only suggests this if the year is right
+   date = QDate(2023, 10, 1);
+   final = utilDetectMatches(date, matches, missing);
+   QCOMPARE(final.size(), 0);
+
+   // Advance to Jan 2025; check it doesn't suggest adding January, since Dec
+   // isn't there. But it should suggest adding 2025
+   date = QDate(2025, 1, 1);
+   final = utilDetectMatches(date, matches, missing);
+   QCOMPARE(final.size(), 0);
+   QCOMPARE(missing.size(), 1);
+   QCOMPARE(missing[0], "bills/2025");
+
+   // Advance to Feb 2025; check that it suggests adding 2025
+   date = QDate(2025, 2, 1);
+   final = utilDetectMatches(date, matches, missing);
+   QCOMPARE(final.size(), 0);
+   QCOMPARE(missing.size(), 1);
+   QCOMPARE(missing[0], "bills/2025");
+
+   // Make to Jan 2025; now add Dec24 and check that it suggests adding Jan25
+   date = QDate(2025, 1, 1);
+   matches << "bills/2024/12dec";
+   final = utilDetectMatches(date, matches, missing);
+   QCOMPARE(final.size(), 0);
+   qDebug() << "missing" << missing;
+   QCOMPARE(missing.size(), 1);
+   QCOMPARE(missing[0], "bills/2025/01jan");
+
+   // Now move to 2026 and make sure it suggests to add that
+   matches << "bills/2025";
+   date = QDate(2026, 1, 1);
+   final = utilDetectMatches(date, matches, missing);
+   QCOMPARE(final.size(), 0);
+   QCOMPARE(missing.size(), 1);
+   QCOMPARE(missing[0], "bills/2026");
 }
 
 QTEST_MAIN(TestPaperman)
