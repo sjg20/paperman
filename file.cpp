f43cd99ef6a0e71c2d0fc831736c161a139de6f8
Simon Glass <sjg@chromium.org>
2024-09-08 16:31:00 -0600

    Deal with QImage::sizeInBytes() versus byteCount()
    The old byteCount() is still used in some older distros. Keep it around
    so that they can build.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- file.cpp
+++ file.cpp
@@ -810,8 +810,14 @@ err_info *File::copyTo (File *fnew, int odd_even, Operation &op, bool verbose)
       if (image.isNull ())
          continue;
 
+      int image_size;
+#if QT_VERSION >= 0x050a00
+      image_size = image.sizeInBytes();
+#else
+      image_size = image.byteCount();
+#endif
       QByteArray ba = QByteArray::fromRawData ((const char *)image.bits (),
-                                               image.sizeInBytes ());
+                                               image_size);
 
 //       int stride = (trueSize.width () * bpp + 7) / 8;
       int stride = image.bytesPerLine ();
@@ -1071,7 +1077,13 @@ QByteArray Filepage::copyData (bool invert, bool force_24bpp) const
 #ifdef USE_24BPP
       image = image.convertToFormat (QImage::Format_RGB888);
       data = image.bits ();
-      size = image.sizeInBytes ();
+      int image_size;
+#if QT_VERSION >= 0x050a00
+      image_size = image.sizeInBytes();
+#else
+      image_size = image.byteCount();
+#endif
+      size = image_size;
       stride = image.bytesPerLine ();
 #else
       stride = stride8;
@@ -1147,20 +1159,26 @@ QByteArray Filepage::getThumbnailRaw (bool invert, QImage &image,
 //    qDebug () << image.width () << image.bytesPerLine ();
 //    image.save ("/tmp/1.jpg");
    QByteArray ba;
-   ba.reserve (image.sizeInBytes ());
+   int image_size;
+#if QT_VERSION >= 0x050a00
+   image_size = image.sizeInBytes();
+#else
+   image_size = image.byteCount();
+#endif
+   ba.reserve(image_size);
    QBuffer buffer(&ba);
    buffer.open(QIODevice::WriteOnly);
    int stride = image.bytesPerLine ();
    int req_stride = image.width () * image.depth () / 8;
    if (word_align || stride == req_stride)
-      buffer.write ((const char *)image.bits (), image.sizeInBytes ());
+      buffer.write ((const char *)image.bits (), image_size);
    else
       {
       // we must copy line by line
       for (int y = 0; y < image.height (); y++)
          buffer.write ((const char *)image.scanLine (y), req_stride);
       }
-   //qDebug () << "thumbnail" << image.sizeInBytes () << ba.size ();
+   //qDebug () << "thumbnail" << image_size << ba.size ();
    return ba;
    }
 
