sjg20/paperman

commit 74cd56d8a837fb7ae4a1616b0e464206d4658a3d

folderlist: Move checkFolders() to folderlist
Move this function into the Folderlist class. Move the timer and the
_awaiting_user flag also.

Signed-off-by: Simon Glass <sjg@chromium.org>

folderlist.cpp
@@ -5,6 +5,7 @@
 #include <QKeyEvent>
 #include <QScrollBar>
 #include <QStandardItemModel>
+#include <QTimer>
 #include "folderlist.h"
 #include "foldersel.h"
 #include "mainwidget.h"
@@ -18,6 +19,7 @@ Folderlist::Folderlist(Foldersel *foldersel, QWidget *parent)
    _model = new QStandardItemModel(1, 1, parent);
    setModel(_model);
    _valid = false;
+   _awaiting_user = false;
 
    horizontalHeader()->hide();
    verticalHeader()->hide();
@@ -35,6 +37,10 @@ Folderlist::Folderlist(Foldersel *foldersel, QWidget *parent)
 
    connect(this, SIGNAL(keypressReceived(QKeyEvent *)),
            this, SLOT(keypressFromFolderList(QKeyEvent *)));
+
+   _timer = new QTimer (this);
+   connect (_timer, SIGNAL(timeout()), this, SLOT(checkFolders()));
+   _timer->start(200); // check 5 times a second
 }
 
 Folderlist::~Folderlist()
@@ -152,6 +158,25 @@ void Folderlist::searchForFolders(const QString& match)
       setFocus();
 
    _valid = folders.size() > 0;
+}
+
+void Folderlist::checkFolders()
+{
+   // close the folder list if the focus is in anything other than the two
+   // fields dealing with the list of folders
+   if (!_awaiting_user && QApplication::focusWidget() != _foldersel &&
+       QApplication::focusWidget() != this && isVisible()) {
+      hide();
+      if (focusWidget() == this)
+         _foldersel->setFocus(Qt::OtherFocusReason);
+   }
+
+   // show the folder list if focus is in folderName
+   if (_valid && QApplication::focusWidget() == _foldersel &&
+       !_main->isScanning()) {
+      showFolders();
+      setFocus(Qt::OtherFocusReason);
+   }
 }
 
 Foldersel::Foldersel(QWidget* parent)

folderlist.h
@@ -18,9 +18,6 @@ public:
    // Return the currently selected item, or -1 if none
    QModelIndex selected();
 
-   /** Show the folders list, sizing it correctly */
-   void showFolders();
-
    // possible missing directories shown to the user
    QStringList _missing;
 
@@ -30,6 +27,9 @@ public:
    // true if the folders list has been set up
    bool _valid;
 
+   // true if waiting for the user to confirm directory creation
+   bool _awaiting_user;
+
    void setMainwidget(Mainwidget *main);
 
    /** Search for folders which match a string */
@@ -37,6 +37,9 @@ public:
 
 public slots:
    void keypressFromFolderList(QKeyEvent *evt);
+
+    //! hide or show the folders list
+    void checkFolders();
 
 signals:
    void keypressReceived(QKeyEvent *event);
@@ -46,10 +49,16 @@ protected:
    virtual void keyPressEvent(QKeyEvent *event) override;
    virtual void mousePressEvent(QMouseEvent *e) override;
 
+   /** Show the folders list, sizing it correctly */
+   void showFolders();
+
    Foldersel *_foldersel;
    QStandardItemModel *_model;
    Mainwidget *_main;
    QWidget *_parent;
+
+   // timer for checking whether we should close the folder list
+   QTimer *_timer;
 };
 
 #endif // FOLDERLIST_H

pscan.cpp
@@ -182,7 +182,6 @@ void Pscan::init()
     _do_preset_check = false;
 
     _searching = false;
-    _awaiting_user = false;
 
     setupBright ();
 
@@ -217,10 +216,6 @@ void Pscan::init()
     QObject::connect(new QShortcut(QKeySequence(Qt::ALT + Qt::Key_L), this,
                                    nullptr, nullptr, Qt::ApplicationShortcut),
                      &QShortcut::activated, this, &Pscan::toggleLetter);
-
-    _folders_timer = new QTimer (this);
-    connect (_folders_timer, SIGNAL(timeout()), this, SLOT(checkFolders()));
-    _folders_timer->start(200); // check 5 times a second
 }
 
 
@@ -254,27 +249,8 @@ void Pscan::closing()
 void Pscan::scanStarting()
 {
    // close the folders list
-   if (!_awaiting_user)
+   if (!_folders->_awaiting_user)
       _folders->hide();
-}
-
-void Pscan::checkFolders()
-{
-   // close the folder list if the focus is in anything other than the two
-   // fields dealing with the list of folders
-   if (!_awaiting_user && QApplication::focusWidget() != folderName &&
-       QApplication::focusWidget() != _folders && _folders->isVisible()) {
-      _folders->hide();
-      if (focusWidget() == _folders)
-         folderName->setFocus(Qt::OtherFocusReason);
-   }
-
-   // show the folder list if focus is in folderName
-   if (_folders->_valid && QApplication::focusWidget() == folderName &&
-       !_main->isScanning()) {
-      _folders->showFolders();
-      _folders->setFocus(Qt::OtherFocusReason);
-   }
 }
 
 void Pscan::scannerChanged (QScanner *scanner)
@@ -317,7 +293,7 @@ void Pscan::scannerChanged (QScanner *scanner)
     setupBright ();
     if (_do_preset_check)
        presetCheck();
-    checkFolders();
+    _folders->checkFolders();
 }
 
 
@@ -422,13 +398,13 @@ bool Pscan::createMissingDir(int item, QString& fname, QModelIndex& ind)
    bool ok;
 
    // Adding a directory
-   _awaiting_user = true;
+   _folders->_awaiting_user = true;
    ok = QMessageBox::question(
       0,
       tr("Confirmation -- Paperman"),
       tr("Do you want to add directory %1").arg(fname),
       QMessageBox::Ok, QMessageBox::Cancel) == QMessageBox::Ok;
-   _awaiting_user = false;
+   _folders->_awaiting_user = false;
    if (!ok)
       return false;
 
@@ -460,7 +436,7 @@ void Pscan::scan_clicked()
 {
    QString dir_path;
 
-   if (_awaiting_user)
+   if (_folders->_awaiting_user)
       return;
    if (_folders->_valid && _folders->isVisible()) {
       QModelIndex ind = _folders->selected();

pscan.h
@@ -112,7 +112,6 @@ public slots:
     void on_config_clicked();
     void on_stop_clicked ();
     void on_folderName_textChanged(const QString &text);
-    void checkFolders(void);
 
     /**
      * @brief Select a directory from the folder list
@@ -157,12 +156,6 @@ protected:
 
     // string which we saw while in the middle of searching
     QString _next_search;
-
-    // timer for checking whether we should close the folder list
-    QTimer *_folders_timer;
-
-    // true if waiting for the user to confirm directory creation
-    bool _awaiting_user;
 
     virtual void languageChange();
 

commit 944073ca74c2a0a4596badaa9b4748d752c71c7f

folderlist: Correct handling of focus
When checkFolders() was moved to Folderlist the focusWidget() call was
not updated. As a result the cursor gets stuck in the folderName field
and clicking in the stackName field doesn't move the focus.

Fix this by updating the call.

Signed-off-by: Simon Glass <sjg@chromium.org>
Fixes: 74cd56d ("folderlist: Move checkFolders() to folderlist")

folderlist.cpp
@@ -173,7 +173,7 @@ void Folderlist::checkFolders()
    if (!_awaiting_user && QApplication::focusWidget() != _foldersel &&
        QApplication::focusWidget() != this && isVisible()) {
       hide();
-      if (focusWidget() == this)
+      if (_parent->focusWidget() == this)
          _foldersel->setFocus(Qt::OtherFocusReason);
    }
 