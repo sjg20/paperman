cb12fa66ee15c0dc605c9bc843672062afe17716
Simon Glass <sjg@chromium.org>
2024-09-28 10:41:20 -0600

    test: Test creating a main window
    File out the startup test.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/test_ops.cpp
+++ test/test_ops.cpp
@@ -3,8 +3,15 @@
 #include "../utils.h"
 #include "test.h"
 
+#include "mainwindow.h"
 #include "test_ops.h"
+
+const char TEST_DIR[] = "test/files";
 
 void TestOps::testStartup()
 {
+   Mainwindow *me;
+
+   me = new Mainwindow();
+   delete me;
 }
