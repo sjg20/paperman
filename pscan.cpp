sjg20/paperman

commit 5c4200ae9d4f5fc6ac08cc79764a7f27ac72d391

pscan: Provide keyboard shortcuts to select paper size
Use Alt-A to select A4 and Alt-L to toggle between US letter and legal.
This makes it faster to choose paper size.

Signed-off-by: Simon Glass <sjg@chromium.org>

pscan.cpp
@@ -226,6 +226,13 @@ void Pscan::init()
                                    nullptr, nullptr, Qt::ApplicationShortcut),
                      &QShortcut::activated, this, &Pscan::presetShortcut5);
 
+    QObject::connect(new QShortcut(QKeySequence(Qt::ALT + Qt::Key_A), this,
+                                   nullptr, nullptr, Qt::ApplicationShortcut),
+                     &QShortcut::activated, this, &Pscan::selectA4);
+    QObject::connect(new QShortcut(QKeySequence(Qt::ALT + Qt::Key_L), this,
+                                   nullptr, nullptr, Qt::ApplicationShortcut),
+                     &QShortcut::activated, this, &Pscan::toggleLetter);
+
     _folders_timer = new QTimer (this);
     connect (_folders_timer, SIGNAL(timeout()), this, SLOT(checkFolders()));
     _folders_timer->start(200); // check 5 times a second
@@ -550,7 +557,10 @@ void Pscan::setPreviewWidget( PreviewWidget *widget )
    int i = 0;
 
    pageSize->clear ();
-   _default_papersize_id = widget->getPreDefA4 ();
+   _papersize_a4 = widget->getPreDefA4();
+   _papersize_letter = widget->getPreDefLetter();
+   _papersize_legal = widget->getPreDefLegal();
+   _default_papersize_id = _papersize_a4;
 
    QProcess paperconf;
    paperconf.start("paperconf", QStringList());
@@ -562,9 +572,9 @@ void Pscan::setPreviewWidget( PreviewWidget *widget )
 
           // We should really look this up by name.
           if (size == "letter")
-              _default_papersize_id = widget->getPreDefLetter ();
+              _default_papersize_id = _papersize_letter;
           else
-              _default_papersize_id = widget->getPreDefA4 ();
+              _default_papersize_id = _papersize_a4;
       }
    }
 
@@ -847,6 +857,19 @@ void Pscan::presetShortcut4()
 void Pscan::presetShortcut5()
 {
    presetShortcut(5);
+}
+
+void Pscan::selectA4()
+{
+   pageSize->setCurrentIndex(_papersize_a4);
+}
+
+void Pscan::toggleLetter()
+{
+   if (pageSize->currentIndex() ==_papersize_letter)
+      pageSize->setCurrentIndex(_papersize_legal);
+   else
+      pageSize->setCurrentIndex(_papersize_letter);
 }
 
 Presetadd::Presetadd(QWidget* parent, Qt::WindowFlags fl)

pscan.h
@@ -143,6 +143,12 @@ public slots:
      */
     void selectDir(const QModelIndex& target);
 
+    // Select A4 paper-size
+    void selectA4();
+
+    // Toggle between US letter and legal
+    void toggleLetter();
+
 protected:
     void reject();
 
@@ -189,6 +195,11 @@ protected:
     bool _awaiting_user;
 
     virtual void languageChange();
+
+    // papersize elements from PreviewWidget::predefs[]
+    int _papersize_a4;
+    int _papersize_letter;
+    int _papersize_legal;
 
 private:
     void init();

commit 536f772966d979faa46bd103f6eb24946a733460

pscan: Updating the scanner when changing paper size
The keyboard-shortcuts don't actually update the pape size at present,
just the combobox which shows it. Call setSize() to do this.

Signed-off-by: Simon Glass <sjg@chromium.org>
Fixes: 5c4200a ("pscan: Provide keyboard shortcuts to select paper size")

pscan.cpp
@@ -757,14 +757,19 @@ void Pscan::presetShortcut5()
 void Pscan::selectA4()
 {
    pageSize->setCurrentIndex(_papersize_a4);
+   _preview->setSize(_papersize_a4);
 }
 
 void Pscan::toggleLetter()
 {
+   int id;
+
    if (pageSize->currentIndex() ==_papersize_letter)
-      pageSize->setCurrentIndex(_papersize_legal);
+      id = _papersize_legal;
    else
-      pageSize->setCurrentIndex(_papersize_letter);
+      id = _papersize_letter;
+   pageSize->setCurrentIndex(id);
+   _preview->setSize(id);
 }
 
 Presetadd::Presetadd(QWidget* parent, Qt::WindowFlags fl)