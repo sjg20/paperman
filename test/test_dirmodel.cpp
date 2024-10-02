0a94aa4e7b1d218de049e6690b9f9e3d418c2e17
Simon Glass <sjg@chromium.org>
2024-10-02 17:12:12 -0600

    test_dirmodel: Update code style
    Change the code style to suit.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/test_dirmodel.cpp
+++ test/test_dirmodel.cpp
@@ -7,44 +7,42 @@
 #include "test_dirmodel.h"
 
 void TestDirmodel::testBase()
-   {
+{
    QString dir = "desktopl";
    QString dir2 = "test";
    Dirmodel model;
    QModelIndex parent, ind;
 
-   model.addDir (dir);
-   model.addDir (dir2);
-   parent = QModelIndex ();
-   printf ("%s\n", model.data (parent, Qt::DisplayRole).toString ().
-           toLatin1 ().constData());
-   int rows = model.rowCount (parent);
-   for (int i = 0; i < rows; i++)
-      {
-      ind = model.index (i, 0, parent);
-      printf ("   %s %p\n", model.data (ind, Qt::DisplayRole).toString ().
-              toLatin1 ().constData(), ind.internalPointer ());
-      if (model.parent (ind) != QModelIndex ())
-         {
-         printf ("   - root parent bad\n");
+   model.addDir(dir);
+   model.addDir(dir2);
+   parent = QModelIndex();
+   printf("%s\n", model.data(parent, Qt::DisplayRole).toString().
+          toLatin1().constData());
+   int rows = model.rowCount(parent);
+   qDebug() << "rows" << rows;
+   for (int i = 0; i < rows; i++) {
+      ind = model.index(i, 0, parent);
+      printf("   %s %p\n", model.data(ind, Qt::DisplayRole).toString().
+             toLatin1().constData(), ind.internalPointer());
+      if (model.parent(ind) != QModelIndex()) {
+         printf("   - root parent bad\n");
          return;
-         }
-      if (ind.row () != i)
-         qDebug () << "row is" << ind.row () << "should be" << i;
-      int rows2 = model.rowCount (ind);
-      for (int j = 0; j < rows2; j++)
-         {
-         QModelIndex ind2 = model.index (j, 0, ind);
-         printf ("       %s %p\n", model.data (ind2, Qt::DisplayRole).
-                 toString ().toLatin1 ().constData(),
-            ind2.internalPointer ());
-         if (model.parent (ind2) != ind)
-            {
-            qDebug () << "   - parent bad, is" << model.parent (ind2) << "should be" << ind;
+      }
+      if (ind.row() != i)
+         qDebug() << "row is" << ind.row() << "should be" << i;
+      int rows2 = model.rowCount(ind);
+      qDebug() << "rows2" << rows2;
+      for (int j = 0; j < rows2; j++) {
+         QModelIndex ind2 = model.index(j, 0, ind);
+         printf("       %s %p\n", model.data(ind2, Qt::DisplayRole).
+                toString().toLatin1().constData(), ind2.internalPointer());
+         if (model.parent(ind2) != ind) {
+            qDebug() << "   - parent bad, is" << model.parent(ind2)
+                     << "should be" << ind;
             return;
-            }
-         if (ind2.row () != j)
-            qDebug () << "row is" << ind2.row () << "should be" << j;
          }
+         if (ind2.row() != j)
+            qDebug() << "row is" << ind2.row() << "should be" << j;
       }
    }
+}
