50c172dda9cc834cf8f2ed6437a254107c4b55eb
Simon Glass <sjg@chromium.org>
2024-10-03 08:48:49 -0600

    test_dirmodel: Use assertions
    Convert this test to use assertions raht erthan just printing things
    out.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/test_dirmodel.cpp
+++ test/test_dirmodel.cpp
@@ -16,32 +16,25 @@ void TestDirmodel::testBase()
    model.addDir(dir);
    model.addDir(dir2);
    parent = QModelIndex();
-   qDebug() << model.data(parent, Qt::DisplayRole).toString();
+//   qDebug() << model.data(parent, Qt::DisplayRole).toString();
    int rows = model.rowCount(parent);
-   qDebug() << "rows" << rows;
+   QCOMPARE(rows, 2);
    for (int i = 0; i < rows; i++) {
       ind = model.index(i, 0, parent);
+
+      QCOMPARE(model.data(ind, Qt::DisplayRole).toString(), "test");
       qDebug() << "   " << model.data(ind, Qt::DisplayRole).toString()
                << ind.internalPointer();
-      if (model.parent(ind) != QModelIndex()) {
-         qDebug() << "   - root parent bad";
-         return;
-      }
-      if (ind.row() != i)
-         qDebug() << "row is" << ind.row() << "should be" << i;
+      QCOMPARE(model.parent(ind), QModelIndex());
+      QCOMPARE(ind.row(), i);
       int rows2 = model.rowCount(ind);
       qDebug() << "rows2" << rows2;
       for (int j = 0; j < rows2; j++) {
          QModelIndex ind2 = model.index(j, 0, ind);
          qDebug() << "       " << model.data(ind2, Qt::DisplayRole).toString()
                   << ind2.internalPointer();
-         if (model.parent(ind2) != ind) {
-            qDebug() << "   - parent bad, is" << model.parent(ind2)
-                     << "should be" << ind;
-            return;
-         }
-         if (ind2.row() != j)
-            qDebug() << "row is" << ind2.row() << "should be" << j;
+         QCOMPARE(model.parent(ind2), ind);
+         QCOMPARE(ind2.row(), j);
       }
    }
 }
