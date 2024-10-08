9edb9ca8ce86a6443e136677ccdb48668339b5d4
Simon Glass <sjg@chromium.org>
2024-10-08 07:14:40 -0600

    test_dirmodel: Add a few more checks
    Check the model and a few other things that seem important.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/test_dirmodel.cpp
+++ test/test_dirmodel.cpp
@@ -23,18 +23,29 @@ void TestDirmodel::checkModel(const QAbstractItemModel *model)
 //   qDebug() << model->data(parent, Qt::DisplayRole).toString();
    int rows = model->rowCount(parent);
    QCOMPARE(rows, 2);
+   QCOMPARE(model->columnCount(parent), 1);
+   QCOMPARE(model->parent(parent), QModelIndex());
+   QCOMPARE(model->data(parent, Qt::DisplayRole).toString(), "");
 
    ind = model->index(0, 0, parent);
+   QCOMPARE(ind.row(), 0);
+   QCOMPARE(ind.column(), 0);
+   QCOMPARE(ind.model(), model);
+   QCOMPARE(ind.parent(), QModelIndex());
    QCOMPARE(model->data(ind, Qt::DisplayRole).toString(), "dir");
 
    ind = model->index(1, 0, parent);
    QCOMPARE(model->data(ind, Qt::DisplayRole).toString(), "other");
+   QCOMPARE(ind.row(), 1);
+   QCOMPARE(ind.column(), 0);
+   QCOMPARE(ind.model(), model);
 
    for (int i = 0; i < rows; i++) {
       ind = model->index(i, 0, parent);
 
       QCOMPARE(model->parent(ind), QModelIndex());
       QCOMPARE(ind.row(), i);
+      QCOMPARE(ind.model(), model);
       int rows2 = model->rowCount(ind);
       QCOMPARE(rows2, i ? 1 : 2);
       for (int j = 0; j < rows2; j++) {
@@ -44,6 +55,7 @@ void TestDirmodel::checkModel(const QAbstractItemModel *model)
          QCOMPARE(disp, dirs[i * 2 + j]);
          QCOMPARE(model->parent(ind2), ind);
          QCOMPARE(ind2.row(), j);
+         QCOMPARE(ind2.model(), model);
       }
    }
 }
