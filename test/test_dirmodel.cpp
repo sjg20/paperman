d1f1c628764a751fbf6692001dffe975372c6522
Simon Glass <sjg@chromium.org>
2024-10-08 07:09:27 -0600

    test_dirmodel: Split checking into a separate function
    We want to test the proxy as well as the model. As a first step move the
    checking code into its own function.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- test/test_dirmodel.cpp
+++ test/test_dirmodel.cpp
@@ -9,8 +9,6 @@
 void TestDirmodel::testBase()
 {
    Dirmodel model;
-   QModelIndex parent, ind;
-   QStringList dirs{"one", "two", "three"};
 
    auto path = setupRepo();
 
@@ -19,31 +17,40 @@ void TestDirmodel::testBase()
    newpath = path + "/other";
    model.addDir(newpath);
 
+   checkModel(&model);
+}
+
+void TestDirmodel::checkModel(const QAbstractItemModel *model)
+{
+   QStringList dirs{"one", "two", "three"};
+   QModelIndex parent, ind;
+
    parent = QModelIndex();
-//   qDebug() << model.data(parent, Qt::DisplayRole).toString();
-   int rows = model.rowCount(parent);
+//   qDebug() << model->data(parent, Qt::DisplayRole).toString();
+   int rows = model->rowCount(parent);
    QCOMPARE(rows, 2);
 
-   ind = model.index(0, 0, parent);
-   QCOMPARE(model.data(ind, Qt::DisplayRole).toString(), "dir");
+   ind = model->index(0, 0, parent);
+   QCOMPARE(model->data(ind, Qt::DisplayRole).toString(), "dir");
 
-   ind = model.index(1, 0, parent);
-   QCOMPARE(model.data(ind, Qt::DisplayRole).toString(), "other");
+   ind = model->index(1, 0, parent);
+   QCOMPARE(model->data(ind, Qt::DisplayRole).toString(), "other");
 
    for (int i = 0; i < rows; i++) {
-      ind = model.index(i, 0, parent);
+      ind = model->index(i, 0, parent);
 
-      QCOMPARE(model.parent(ind), QModelIndex());
+      QCOMPARE(model->parent(ind), QModelIndex());
       QCOMPARE(ind.row(), i);
-      int rows2 = model.rowCount(ind);
+      int rows2 = model->rowCount(ind);
       QCOMPARE(rows2, i ? 1 : 2);
       for (int j = 0; j < rows2; j++) {
-         QModelIndex ind2 = model.index(j, 0, ind);
-         QString disp = model.data(ind2, Qt::DisplayRole).toString();
+         QModelIndex ind2 = model->index(j, 0, ind);
+         QString disp = model->data(ind2, Qt::DisplayRole).toString();
 
          QCOMPARE(disp, dirs[i * 2 + j]);
-         QCOMPARE(model.parent(ind2), ind);
+         QCOMPARE(model->parent(ind2), ind);
          QCOMPARE(ind2.row(), j);
       }
    }
 }
+
--- test/test_dirmodel.h
+++ test/test_dirmodel.h
@@ -1,6 +1,8 @@
 #ifndef TEST_DIRMODEL_H
 #define TEST_DIRMODEL_H
 
+class QAbstractItemModel;
+
 #include <QObject>
 
 #include "suite.h"
@@ -13,6 +15,13 @@ public:
 
 private slots:
    void testBase();
+
+private:
+   /**
+    * @brief Run checks on a Dirmodel
+    * @param model   Model to check
+    */
+   void checkModel(const QAbstractItemModel *model);
 };
 
 #endif // TEST_DIRMODEL_H
